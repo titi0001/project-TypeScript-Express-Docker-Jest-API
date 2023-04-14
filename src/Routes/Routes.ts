582f4b3c7eea7868b542fb48b14bb50b6753dbb6
Thiago Pereira <titiura@gmail.com>
2023-04-14 14:46:37 -0300

    feat: add todas Rotas de Cars

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -8,4 +8,24 @@ routes.post(
   (req, res, next) => new CarController(req, res, next).create(),
 );
 
+routes.get(
+  '/cars',
+  (req, res, next) => new CarController(req, res, next).create(),
+);
+
+routes.get(
+  '/cars/:id',
+  (req, res, next) => new CarController(req, res, next).create(),
+);
+
+routes.put(
+  '/cars/:id',
+  (req, res, next) => new CarController(req, res, next).create(),
+);
+
+routes.delete(
+  '/cars/:id',
+  (req, res, next) => new CarController(req, res, next).create(),
+);
+
 export default routes;
