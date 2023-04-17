d9a74672df12a86a26885e9e4de1049487e1c90a
Thiago Pereira <titiura@gmail.com>
2023-04-17 08:41:57 -0300

    fix: passando arquivos para singular

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,6 +1,8 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
-import CarsODM from '../Models/CarsODM';
+import CarsODM from '../Models/CarODM';
+
+const NOT_FOUND = 'Car not found';
 
 export default class CarService {
   private model:CarsODM = new CarsODM();
@@ -15,5 +17,29 @@ export default class CarService {
   async createNewCar(car: ICar) {
     const newCar = await this.model.create(car);
     return this.createCarDomain(newCar);
+  }
+
+  async readAll() {
+    const cars = await this.model.getAll();
+    const allCars = cars.map((car) => this.createCarDomain(car));
+    return allCars;
+  }
+
+  async readById(id: string) {
+    const carById = await this.model.getById(id);
+    if (!carById) throw new Error(NOT_FOUND);   
+    return this.createCarDomain(carById);
+  }
+
+  async update(id: string, car: Partial<ICar>) {
+    const updateCar = await this.model.update(id, car);
+    if (updateCar) return this.createCarDomain(updateCar);
+    throw new Error(NOT_FOUND);
+  }
+
+  async remove(id: string) {
+    const removeCar = await this.model.remove(id);
+    if (removeCar) return this.createCarDomain(removeCar);
+    throw new Error(NOT_FOUND);
   }
 }
