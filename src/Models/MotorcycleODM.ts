3f0f409de5b634fcbe4c9e2689707c3118c0864d
Thiago Pereira <titiura@gmail.com>
2023-04-17 08:43:20 -0300

    fix: alteração para singular no arquivo

--- src/Models/MotorcycleODM.ts
+++ src/Models/MotorcycleODM.ts
@@ -1,10 +1,10 @@
 import { Schema } from 'mongoose';
 import AbstractODM from './AbstractODM';
-import IMotorcycles from '../Interfaces/IMotorcycles';
+import IMotorcycle from '../Interfaces/IMotorcycle';
 
-export default class MotorcyclesODM extends AbstractODM<IMotorcycles> {
+export default class MotorcycleODM extends AbstractODM<IMotorcycle> {
   constructor() {
-    const schema = new Schema<IMotorcycles>({
+    const schema = new Schema<IMotorcycle>({
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
