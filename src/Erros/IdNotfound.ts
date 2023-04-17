7f77cb06067d9cbc93d29df68941c46f4abe040c
Thiago Pereira <titiura@gmail.com>
2023-04-17 08:42:43 -0300

    fix: refatorando Erros

--- src/Erros/IdNotfound.ts
+++ src/Erros/IdNotfound.ts
@@ -2,6 +2,7 @@ export default class IdNotFound extends Error {
   constructor(message : string) {
     super(message);
     this.message = message;
+    this.name = 'Car not Found';
     this.stack = '404';
   }
 }
--- src/Erros/InvalidId.ts
+++ src/Erros/InvalidId.ts
@@ -2,6 +2,7 @@ export default class InvalidId extends Error {
   constructor(message : string) {
     super(message);
     this.message = message;
+    this.name = 'Invalid mongo id';
     this.stack = '422';
   }
 }
